725c4450f7ec49f05dcb0987edbc13921cac7cf1
翟永超 <dyc87112@qq.com>
2017-01-08 22:16:27 +0800

    《Spring Cloud实战》更新

--- spring_cloud_in_action/api-gateway-consul/src/main/java/com/didispace/Application.java
+++ spring_cloud_in_action/api-gateway-consul/src/main/java/com/didispace/Application.java
@@ -1,10 +1,12 @@
 package com.didispace;
 
-import com.didispace.filter.AccessFilter;
+import com.netflix.zuul.FilterFileManager;
+import com.netflix.zuul.FilterLoader;
+import com.netflix.zuul.groovy.GroovyCompiler;
+import com.netflix.zuul.groovy.GroovyFileFilter;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringCloudApplication
@@ -12,11 +14,16 @@ public class Application {
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(Application.class).web(true).run(args);
+		String zuulFilterRoot = "api-gateway-consul/filter";
+		FilterLoader.getInstance().setCompiler(new GroovyCompiler());
+		try {
+			FilterFileManager.setFilenameFilter(new GroovyFileFilter());
+			FilterFileManager.init(5,
+					zuulFilterRoot + "/pre",
+					zuulFilterRoot + "/post");
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
-
-//	@Bean
-//	public AccessFilter accessFilter() {
-//		return new AccessFilter();
-//	}
 
 }
--- spring_cloud_in_action/feign-consumer/src/main/java/com/didispace/ConsumerApplication.java
+++ spring_cloud_in_action/feign-consumer/src/main/java/com/didispace/ConsumerApplication.java
@@ -1,18 +1,21 @@
 package com.didispace;
 
+import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ConsumerApplication {
+
+//	@Bean
+//	Logger.Level feignLoggerLevel() {
+//		return Logger.Level.FULL;
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumerApplication.class, args);
