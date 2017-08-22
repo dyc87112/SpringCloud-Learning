78e4b4cf42640cc0bab23ff3bd695a7bf03847a1
zongzhankui <zongzhankui@gomeholdings.com>
2017-08-22 17:02:57 +0800

    修改consul-consumer

--- 2-Dalston版教程示例/consul-consumer/src/main/java/com/didispace/DcController.java
+++ 2-Dalston版教程示例/consul-consumer/src/main/java/com/didispace/DcController.java
@@ -2,10 +2,7 @@ package com.didispace;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -25,7 +22,7 @@ public class DcController {
 
     @GetMapping("/consumer")
     public String dc() {
-        ServiceInstance serviceInstance = loadBalancerClient.choose("eureka-client");
+        ServiceInstance serviceInstance = loadBalancerClient.choose("consul-client");
         String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/dc";
         System.out.println(url);
         return restTemplate.getForObject(url, String.class);
