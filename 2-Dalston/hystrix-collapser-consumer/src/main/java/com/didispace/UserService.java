274c03e044eed30e15e9cb8f2803ad38132b1929
Dab <pang2081@163.com>
2018-03-15 15:08:35 +0800

    分布式服务跟踪

--- 2-Dalston/hystrix-collapser-consumer/src/main/java/com/didispace/UserService.java
+++ 2-Dalston/hystrix-collapser-consumer/src/main/java/com/didispace/UserService.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Future;
 
