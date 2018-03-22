56651fd182b0f4ced2f2911b462b2b9f71eba889
Dab <pang2081@163.com>
2018-03-22 17:07:34 +0800

    commmit

--- Chapter1-1-7/config-client-eureka-kafka/src/main/java/com/didispace/ConfigClientEurekaKafkaApplication.java
+++ Chapter1-1-7/config-client-eureka-kafka/src/main/java/com/didispace/ConfigClientEurekaKafkaApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class Application {
+public class ConfigClientEurekaKafkaApplication {
 
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(Application.class).web(true).run(args);
+		new SpringApplicationBuilder(ConfigClientEurekaKafkaApplication.class).web(true).run(args);
 	}
 
 }
--- Chapter1-1-7/config-server-eureka-kafka/src/main/java/com/didispace/ConfigServerEurekaKafkaApplication.java
+++ Chapter1-1-7/config-server-eureka-kafka/src/main/java/com/didispace/ConfigServerEurekaKafkaApplication.java
@@ -8,10 +8,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
-public class Application {
+public class ConfigServerEurekaKafkaApplication {
 
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(Application.class).web(true).run(args);
+		new SpringApplicationBuilder(ConfigServerEurekaKafkaApplication.class).web(true).run(args);
 	}
 
 }
