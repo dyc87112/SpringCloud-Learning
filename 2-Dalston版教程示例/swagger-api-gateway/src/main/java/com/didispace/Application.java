f8a3186f59ac3ba04d1abe61368418cc9dcf771b
zhaiyongchao <dyc87112@qq.com>
2018-05-23 18:46:01 +0800

    利用网关汇总API文档

---
+++ 2-Dalston版教程示例/swagger-api-gateway/src/main/java/com/didispace/Application.java
@@ -0,0 +1,43 @@
+package com.didispace;
+
+import com.spring4all.swagger.EnableSwagger2Doc;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@EnableSwagger2Doc
+@EnableZuulProxy
+@SpringCloudApplication
+public class Application {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(Application.class).web(true).run(args);
+	}
+
+	@Component
+	@Primary
+	class DocumentationConfig implements SwaggerResourcesProvider {
+		@Override
+		public List<SwaggerResource> get() {
+			List resources = new ArrayList<>();
+			resources.add(swaggerResource("service-a", "/swagger-service-a/v2/api-docs", "2.0"));
+			resources.add(swaggerResource("service-b", "/swagger-service-b/v2/api-docs", "2.0"));
+			return resources;
+		}
+
+		private SwaggerResource swaggerResource(String name, String location, String version) {
+			SwaggerResource swaggerResource = new SwaggerResource();
+			swaggerResource.setName(name);
+			swaggerResource.setLocation(location);
+			swaggerResource.setSwaggerVersion(version);
+			return swaggerResource;
+		}
+	}
+}
---
+++ 2-Dalston版教程示例/swagger-service-b/src/main/java/com/didispace/Application.java
@@ -0,0 +1,36 @@
+package com.didispace;
+
+import com.spring4all.swagger.EnableSwagger2Doc;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableSwagger2Doc
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(Application.class).web(true).run(args);
+	}
+
+	@RestController
+	class BbbController {
+
+		@Autowired
+		DiscoveryClient discoveryClient;
+
+		@GetMapping("/service-b")
+		public String dc() {
+			String services = "Services: " + discoveryClient.getServices();
+			System.out.println(services);
+			return services;
+		}
+
+	}
+
+}
