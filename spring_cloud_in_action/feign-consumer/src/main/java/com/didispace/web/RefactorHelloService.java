9d29645ad90bb64baf9e2d7b6584f258ec7d2be0
zhaiyongchao <dyc87112@qq.com>
2018-06-04 16:42:32 +0800

    目录整理

--- spring_cloud_in_action/feign-consumer/src/main/java/com/didispace/web/RefactorHelloService.java
+++
@@ -1,9 +0,0 @@
-package com.didispace.web;
-
-import org.springframework.cloud.netflix.feign.FeignClient;
-
-@FeignClient(value = "HELLO-SERVICE")
-public interface RefactorHelloService extends com.didispace.service.HelloService {
-
-
-}
