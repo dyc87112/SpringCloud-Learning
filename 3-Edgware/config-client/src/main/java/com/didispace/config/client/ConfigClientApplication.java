925fb76373db62f052ce3b6a5c766ee7c3c58dc3
zhaiyongchao <dyc87112@qq.com>
2018-06-05 09:50:33 +0800

    示例：配置中心采用数据库存储

--- 3-Edgware/config-client/src/main/java/com/didispace/config/client/ConfigClientApplication.java
+++ 3-Edgware/config-client/src/main/java/com/didispace/config/client/ConfigClientApplication.java
@@ -1,8 +1,8 @@
 package com.didispace.config.client;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(ConfigClientApplication.class).web(true).run(args);
+        SpringApplication.run(ConfigClientApplication.class);
     }
 
     @RefreshScope
