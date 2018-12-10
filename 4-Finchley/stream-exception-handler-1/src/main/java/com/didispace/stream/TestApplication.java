a7fb2e87a2ccf9179d14ed1fdaea13a2fb0caf48
zhaiyongchao <dyc87112@qq.com>
2018-12-10 17:19:18 +0800

    Spring Cloud Stream消费失败后的处理策略（一）：自动重试

---
+++ 4-Finchley/stream-exception-handler-1/src/main/java/com/didispace/stream/TestApplication.java
@@ -0,0 +1,87 @@
+package com.didispace.stream;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.SubscribableChannel;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@EnableBinding(TestApplication.TestTopic.class)
+@SpringBootApplication
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+
+    @RestController
+    static class TestController {
+
+        @Autowired
+        private TestTopic testTopic;
+
+        /**
+         * 消息生产接口
+         *
+         * @param message
+         * @return
+         */
+        @GetMapping("/sendMessage")
+        public String messageWithMQ(@RequestParam String message) {
+            testTopic.output().send(MessageBuilder.withPayload(message).build());
+            return "ok";
+        }
+
+    }
+
+    /**
+     * 消息消费逻辑
+     */
+    @Slf4j
+    @Component
+    static class TestListener {
+
+        int counter = 1;
+
+        @StreamListener(TestTopic.INPUT)
+        public void receive(String payload) {
+            log.info("Received: " + payload + ", " + counter);
+            throw new RuntimeException("Message consumer failed!");
+
+            // 计数，模拟重试过程中成功消费
+//            if (counter == 3) {
+//                counter = 1;
+//                return;
+//            } else {
+//                counter++;
+//                throw new RuntimeException("Message consumer failed!");
+//            }
+        }
+
+    }
+
+    interface TestTopic {
+
+        String OUTPUT = "example-topic-output";
+        String INPUT = "example-topic-input";
+
+        @Output(OUTPUT)
+        MessageChannel output();
+
+        @Input(INPUT)
+        SubscribableChannel input();
+
+    }
+
+}
