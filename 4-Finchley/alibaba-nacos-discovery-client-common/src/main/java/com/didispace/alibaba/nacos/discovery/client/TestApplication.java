ede1816a50dc28b2cecabed0050fdc196981afee
zhaiyongchao <dyc87112@qq.com>
2019-01-15 17:44:23 +0800

    Spring Cloud Alibaba基础教程：使用Nacos实现服务注册与发现

---
+++ 4-Finchley/alibaba-nacos-discovery-client-common/src/main/java/com/didispace/alibaba/nacos/discovery/client/TestApplication.java
@@ -0,0 +1,41 @@
+package com.didispace.alibaba.nacos.discovery.client;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+
+    @Slf4j
+    @RestController
+    static class TestController {
+
+        @Autowired
+        LoadBalancerClient loadBalancerClient;
+
+        @GetMapping("/test")
+        public String test() {
+            // 通过spring cloud common中的负载均衡接口选取服务提供节点实现接口调用
+            ServiceInstance serviceInstance = loadBalancerClient.choose("alibaba-nacos-discovery-server");
+            String url = serviceInstance.getUri() + "/hello?name=" + "didi";
+            RestTemplate restTemplate = new RestTemplate();
+            String result = restTemplate.getForObject(url, String.class);
+            return "Invoke : " + url + ", return : " + result;
+        }
+    }
+
+}
---
+++ 4-Finchley/alibaba-nacos-discovery-server/src/main/java/com/didispace/alibaba/nacos/discovery/server/TestApplication.java
@@ -0,0 +1,33 @@
+package com.didispace.alibaba.nacos.discovery.server;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+
+    @Slf4j
+    @RestController
+    static class TestController {
+
+        @GetMapping("/hello")
+        public String hello(@RequestParam String name) {
+            return "hello " + name;
+        }
+
+    }
+
+}
