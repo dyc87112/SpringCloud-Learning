7c9b5910364c3fdb0e6f7dce6e0884fb1ade86ef
zhaiyongchao <dyc87112@qq.com>
2019-02-11 09:01:02 +0800

    Spring Cloud Alibaba基础教程：使用Nacos作为配置中心

--- 4-Finchley/alibaba-nacos-config-client/src/main/java/com/didispace/alibaba/nacos/config/client/TestApplication.java
+++ 4-Finchley/alibaba-nacos-config-client/src/main/java/com/didispace/alibaba/nacos/config/client/TestApplication.java
@@ -31,4 +31,28 @@ public class TestApplication {
 
     }
 
+    /**
+     * 多文件加载的例子使用的验证接口
+     *
+     * blog: http://blog.didispace.com/spring-cloud-alibaba-nacos-config-3/
+     */
+    @Slf4j
+    @RestController
+    @RefreshScope
+    static class Test2Controller {
+
+        @Value("${didispace.title:}")
+        private String title;
+        @Value("${aaa:}")
+        private String aaa;
+        @Value("${bbb:}")
+        private String bbb;
+
+        @GetMapping("/test2")
+        public String test2() {
+            return title + ", " + aaa + ", " + bbb;
+        }
+
+    }
+
 }
