d964eb8af7fb903dab66a3b988ee8d8e24ddc7ac
zhaiyongchao <dyc87112@qq.com>
2019-08-16 19:02:48 +0800

    Spring Cloud Alibaba基础教程：与Dubbo的完美融合

---
+++ 4-Finchley/alibaba-dubbo-api/src/main/java/com/didispace/alibaba/dubbo/api/HelloService.java
@@ -0,0 +1,13 @@
+package com.didispace.alibaba.dubbo.api;
+
+/**
+ * Created by 程序猿DD/翟永超 on 2019/8/5.
+ * <p>
+ * Blog: http://blog.didispace.com/
+ * Github: https://github.com/dyc87112/
+ */
+public interface HelloService {
+
+    String hello(String name);
+
+}
